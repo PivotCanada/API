0f92971ce33dee0b8c29fb8b24572a693b68f7e6
Dalyan Parker <dalyan.parker@mail.mcgill.ca>
2020-04-19 00:52:43 +0200

    g

--- app.js
+++ app.js
@@ -55,16 +55,6 @@ app.use("/search", searchRoutes);
 
 //
 
-if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static(path.join(__dirname, "build")));
-
-  // Express serve up index.html file if it doesn't recognize route
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
-
 // Not Found
 
 app.use((req, res, next) => {
