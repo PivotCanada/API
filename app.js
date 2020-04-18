d9fbbfb2f3566d1fb034d95be2ae3ede22930757
Dalyan Parker <dalyan.parker@mail.mcgill.ca>
2020-04-19 00:31:10 +0200

    heroku deployment initial

--- app.js
+++ app.js
@@ -56,10 +56,6 @@ app.use("/search", searchRoutes);
 // Not Found
 
 app.use((req, res, next) => {
-  res.status(200).json("Hey");
-});
-
-app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
   next(error);
